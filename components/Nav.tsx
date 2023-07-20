25e4d978f6550e80707e610136875dbc4710de6e
Abbo Adamou <abbo@abbos-mbp.mynetworksettings.com>
2023-07-20 19:44:48 -0400

    Layout issues fixed

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,8 +7,8 @@ import Image from "next/image";
 
 const Nav = () => {
   return (
-    <nav className="flex h-70 items-center border border-slate-50 border-b-natural-5 dark:border-darkBG-3">
-      <ul className="flex w-full flex-row items-center justify-around">
+    <nav className="flex h-70 items-center border border-slate-50 border-b-natural-5 bg-white dark:border-darkBG-3 dark:bg-darkBG-1">
+      <ul className="padding-layout flex w-full flex-row items-center justify-between">
         <li>
           <Link href="/">
             <Image
