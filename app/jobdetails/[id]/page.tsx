3fe9ea96c085f29f054890158996470446201bb2
Matthew Gajo <matthew.gajo@gmail.com>
2023-07-25 12:35:22 -0400

    Layout finished using dummy components

--- app/jobdetails/[id]/page.tsx
+++ app/jobdetails/[id]/page.tsx
@@ -14,28 +14,35 @@ const page = ({ params }: { params: { id: String } }) => {
     <section className="padding-layout flex flex-col gap-y-9  pb-[90px] pt-[46px]">
       <PageTitle />
       <section>
-        <button className="flex items-center justify-around gap-2 rounded-jobit px-[10px] py-[7px] dark:bg-darkBG-3">
-          <Image
-            src={"/img/icons/cheveron.svg"}
-            priority
-            height={18}
-            width={18}
-            alt={"back arrow icon"}
-          />
-          <a className="pr-1">Back</a>
-        </button>
-        <article className="flex w-2/3 items-center justify-center dark:bg-darkBG-3">
-          <div>
-            <p>Large Job Details Component</p>
-          </div>
-        </article>
-        <article className="flex flex-col">
-          {[...Array(9)].map((i) => (
-            <article key={i}>
-              <h1>Inline Similar jobs Component</h1>
-            </article>
-          ))}
-        </article>
+        <div className="hidden pb-6 ">
+          <button className="flex items-center justify-around gap-2 rounded-jobit px-[10px] py-[7px] dark:bg-darkBG-3">
+            <Image
+              src={"/img/icons/cheveron.svg"}
+              priority
+              height={18}
+              width={18}
+              alt={"back arrow icon"}
+            />
+            <a className="pr-1">Back</a>
+          </button>
+        </div>
+        <section className="flex flex-col gap-6 md:flex-row">
+          <article className="flex h-[1549px] items-center justify-center rounded-jobit bg-white dark:bg-darkBG-3 md:w-2/3">
+            <div>
+              <p>Large Job Details Component</p>
+            </div>
+          </article>
+          <article className="flex flex-col gap-4 md:w-1/3">
+            {[...Array(9)].map((i) => (
+              <article
+                key={i}
+                className="flex h-36 items-center justify-center rounded-jobit bg-white dark:bg-darkBG-3"
+              >
+                <h1>Inline Similar jobs Component</h1>
+              </article>
+            ))}
+          </article>
+        </section>
       </section>
     </section>
   );
