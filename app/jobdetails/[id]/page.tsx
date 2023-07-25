a790dcde0b6f509a99e65f6d391cd6c7b1b76045
Matthew Gajo <matthew.gajo@gmail.com>
2023-07-25 11:41:20 -0400

    Changed default title

--- app/jobdetails/[id]/page.tsx
+++ app/jobdetails/[id]/page.tsx
@@ -1,37 +1,48 @@
-/* eslint-disable camelcase */
-import ErrorFallback from "@components/ErrorFallback";
-import JD_inlineJobCard from "@components/JD_inlineJobCard";
-import JD_jobCardLarge from "@components/JD_JobCardLarge";
-import { JD_inlineJobCardData, JD_jobCardLargeData } from "@data";
+/* eslint-disable tailwindcss/no-custom-classname */
+import { Metadata } from "next";
+import Image from "next/image";
 import React from "react";
-import { ErrorBoundary } from "react-error-boundary";
 
-const jobdetails = ({ params }: { params: { id: String } }) => {
+export const metadata: Metadata = {
+  title: "Jobit - Job Details",
+  description: "Job Finder Web Application",
+};
+
+const page = ({ params }: { params: { id: String } }) => {
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <main className="padding-layout mt-8">
-        <div className="flex flex-col gap-2">
-          <h2 className="headline-2">{"Let's find your dream job"}</h2>
-          {/* Current Date */}
-          <p className="body-8 text-natural-6">
-            {new Date().toLocaleString("en-us", {
-              weekday: "long",
-              month: "short",
-              day: "2-digit",
-              year: "numeric",
-            })}
-          </p>
-          <JD_inlineJobCard data={JD_inlineJobCardData} />
-          <JD_jobCardLarge data={JD_jobCardLargeData} />
-          <section className="flex justify-between">
-            {/* Job Details */}
-
-            {/* Inline Job Cards */}
-          </section>
+    <section className="padding-layout flex flex-col gap-y-9  pb-[90px] pt-[46px]">
+      <div className="body-19 flex w-fit cursor-pointer space-x-[6px] rounded-[10px] bg-natural-2 px-[10px] py-[7px] text-natural-7 dark:bg-darkBG-3 dark:text-natural-6">
+        <Image
+          src={"/img/iconography/arrow-left.svg"}
+          priority
+          height={18}
+          width={18}
+          alt="icon"
+        />
+        <span>Back</span>
+      </div>
+      <div className="flex flex-col gap-[14px] lg:flex-row lg:gap-x-10">
+        <div className="flex w-full max-w-[816px] flex-col gap-y-5">
+          <div className="flex h-full items-center justify-center rounded-xl bg-white dark:bg-darkBG-2">
+            Large Company Details Component
+          </div>
         </div>
-      </main>
-    </ErrorBoundary>
+        <div className="flex w-full flex-col gap-y-5 md:max-w-[400px]">
+          <div className="text-[22px] font-bold leading-8">Similar Jobs</div>
+          <div className="flex flex-col gap-y-6">
+            {[...Array(8)].map((i) => (
+              <span
+                key={i}
+                className="flex h-[90px] w-full items-center justify-center rounded-xl bg-white shadow-1 dark:bg-darkBG-2 md:max-w-[400px]"
+              >
+                Inline Similar Companie Component
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default jobdetails;
+export default page;
