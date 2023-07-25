5550551ec8461369ffa48f8edee3fa00f326170c
Matthew Gajo <matthew.gajo@gmail.com>
2023-07-25 12:16:48 -0400

    Made changes to JobCard to use semantic elements

--- components/Home/Cards/JobCard.tsx
+++ components/Home/Cards/JobCard.tsx
@@ -33,8 +33,8 @@ const JobCard = ({ data }: Props) => {
   const logo = getLogo(employer_name);
   const averagePay = averagePayPerHour(job_min_salary, job_max_salary);
   return (
-    <div className="flex w-full flex-col gap-y-[30px] rounded-jobit bg-white p-5 shadow-1 dark:bg-darkBG-2 lg:max-w-[400px]">
-      <div className="flex items-center gap-5">
+    <article className="flex w-full flex-col gap-y-[30px] rounded-jobit bg-white p-5 shadow-1 dark:bg-darkBG-2 lg:max-w-[400px]">
+      <header className="flex items-center gap-5">
         <div className="flex h-[46px] w-[46px] items-center justify-center  rounded-lg border-[3px] border-natural-3 bg-natural-2 dark:border-logoDark dark:bg-logoDark lg:h-[64px] lg:w-[64px]">
           <div className="relative h-[34.5px] w-[34.5px] lg:h-12 lg:w-12">
             <Image
@@ -46,7 +46,7 @@ const JobCard = ({ data }: Props) => {
             />
           </div>
         </div>
-        <div className="flex h-[60px] flex-1 flex-col justify-between lg:h-16">
+        <section className="flex h-[60px] flex-1 flex-col justify-between lg:h-16">
           <div className="flex items-start justify-between">
             <h2 className="body-6 lg:body-2 line-clamp-1 text-black dark:text-white lg:max-w-[250px]">
               {job_title}
@@ -67,12 +67,12 @@ const JobCard = ({ data }: Props) => {
                 <Badge key={technology} style={"btn-tag"} title={technology} />
               ))}
           </div>
-        </div>
-      </div>
-      <p className="lg:body-8 body-12 line-clamp-6 text-natural-7 dark:text-natural-6">
+        </section>
+      </header>
+      <summary className="lg:body-8 body-12 line-clamp-6 text-natural-7 dark:text-natural-6">
         {job_description}
-      </p>
-      <div className="flex justify-between">
+      </summary>
+      <section className="flex justify-between">
         <Badge
           style={"btn-tag-icon"}
           title={`${getEmployementType(
@@ -90,8 +90,8 @@ const JobCard = ({ data }: Props) => {
           title={`${getSincePostedDate(job_posted_at_datetime_utc)}`}
           icon={"/img/iconography/clock.svg"}
         />
-      </div>
-      <div className="flex items-center justify-between">
+      </section>
+      <section className="flex items-center justify-between">
         {averagePay !== "0" ? (
           <span className="body-6 lg:body-2 text-black dark:text-white">
             ${averagePay}
@@ -112,8 +112,8 @@ const JobCard = ({ data }: Props) => {
           style={"btn-primary lg:py-[14px] px-[14px] py-2 lg:px-3"}
           title={"Apply Now"}
         />
-      </div>
-    </div>
+      </section>
+    </article>
   );
 };
 
