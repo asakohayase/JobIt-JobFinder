ba950ea391af75abd55b71ad737b720d70b7acde
Ryan Carpenter <scrian_12@yahoo.com>
2023-07-29 15:39:38 -0400

    layout page for company details

---
+++ app/estimatedsalaries/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Jobit - Estimated Salaries",
+  description: "Job Finder Web Application",
+};
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
--- tailwind.config.js
+++ tailwind.config.js
@@ -38,6 +38,7 @@ module.exports = {
       },
       borderRadius: {
         jobit: "10px",
+        input: "20px",
       },
     },
   },
