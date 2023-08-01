f0c663e2d11b39fa93b85ba5dc7eaaab8363dcd2
Matthew Gajo <matthew.gajo@gmail.com>
2023-07-31 21:16:17 -0400

    saving before changing

--- app/example/page.tsx
+++ app/example/page.tsx
@@ -2,62 +2,72 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   BarElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-// export const options = {
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       display: false,
-//       position: "top" as const,
-//     },
-//     title: {
-//       display: false,
-//       text: "Estimated Salary for NodeJS Developer in New York",
-//     },
-//   },
-// };
+export const options1 = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+      position: "top" as const,
+    },
+    title: {
+      display: false,
+      text: "Estimated Salary for NodeJS Developer in New York",
+    },
+  },
+};
 
-// const labels = ["Salary.com", "ZipRecruiter", "Talent.com"];
+const labels = ["Salary.com", "ZipRecruiter", "Talent.com"];
 
-// export const data = {
-//   labels,
-//   datasets: [
-//     {
-//       label: "Minimum salary",
-//       borderRadius: 30,
-//       data: [72000, 84000, 77500],
-//       backgroundColor: "rgba(253,221,140, 1)",
-//       barThickness: 10,
-//     },
-//     {
-//       label: "Maximum salary",
-//       borderRadius: 30,
-//       data: [224000, 250000, 200000],
-//       backgroundColor: "rgba(11,171,124, 0.7)",
-//       barThickness: 10,
-//     },
-//     {
-//       label: "median salary",
-//       borderRadius: 30,
-//       data: [142000, 172000, 150000],
-//       backgroundColor: "rgb(255,187,215)",
-//       barThickness: 10,
-//     },
-//   ],
-// };
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Minimum salary",
+      borderRadius: 30,
+      data: [72000, 84000, 77500],
+      backgroundColor: "rgba(253,221,140, 1)",
+      barThickness: 10,
+    },
+    {
+      label: "Maximum salary",
+      borderRadius: 30,
+      data: [224000, 250000, 200000],
+      backgroundColor: "rgba(11,171,124, 0.7)",
+      barThickness: 10,
+    },
+    {
+      label: "median salary",
+      borderRadius: 30,
+      data: [142000, 172000, 150000],
+      backgroundColor: "rgb(255,187,215)",
+      barThickness: 10,
+    },
+  ],
+};
 
 const page = () => {
-  const options = {
+  const data1 = {
     options: {
       chart: {
         id: "basic-bar",
@@ -80,12 +90,12 @@ const page = () => {
           <h1 className="headline-1">
             Estimated Salary for NodeJS Developer in New York
           </h1>
-          {/* <Bar options={options} data={data} className="" /> */}
+          <Bar options={options1} data={data} className="" />
           <Chart
-            options={options.options}
-            series={options.series}
+            options={data1.options}
+            series={data1.series}
             type="bar"
-            width="500'"
+            width="500"
           />
         </figure>
       </section>
