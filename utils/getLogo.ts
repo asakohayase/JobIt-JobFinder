2fb04ca1b5e99d2ae3cde74e5826de082dfdfddd
Matthew Gajo <matthew.gajo@gmail.com>
2023-08-02 19:16:11 -0400

    creates logo if there isnt one we already have

--- utils/getLogo.ts
+++ utils/getLogo.ts
@@ -1,4 +1,26 @@
+import { generateSync } from "text-to-image";
+
 import { companyInfo } from "./companyInfo";
+
+function generateLogo(name: string) {
+  const firstLetters = name
+    .split(" ", 2)
+    .map((substring) => substring.charAt(0));
+
+  const joinedLetters = firstLetters.join("");
+
+  const uri = generateSync(joinedLetters, {
+    bgColor: "#0BAB7C",
+    textColor: "rgba(0,0,0,1)",
+    fontSize: 20,
+    fontFamily: "sans-serif",
+    maxWidth: 45,
+    verticalAlign: "center",
+    textAlign: "center",
+  });
+
+  return uri;
+}
 
 export const getLogo = (name: string) => {
   const company = Object.keys(companyInfo).filter((key) =>
@@ -7,7 +29,7 @@ export const getLogo = (name: string) => {
 
   const logo =
     companyInfo[<keyof typeof companyInfo>company[0]]?.logo ??
-    "/img/company-logo/adobeXD.svg";
+    generateLogo(name);
 
   return logo;
 };
